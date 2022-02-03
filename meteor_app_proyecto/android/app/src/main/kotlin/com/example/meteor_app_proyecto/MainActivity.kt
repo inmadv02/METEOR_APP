ee6944b00f6685dd88da2590016b3df17a1bced5
María Inmaculada Domínguez Vargas <dominguez.vamar21@triana.salesianos.edu>
2022-02-03 13:43:19 +0100

    estilos

---
+++ meteor_app_proyecto/android/app/src/main/kotlin/com/example/meteor_app_proyecto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.meteor_app_proyecto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ meteor_app_proyecto/lib/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Styles {
+  static const Color moradoApagado = Color.fromRGBO(130, 140, 174, 1);
+  static const Color gris = Color.fromRGBO(204, 207, 218, 1);
+  static const Color azulIconsMenu = Color.fromRGBO(85, 126, 174, 1);
+  static const Color azulMenu = Color.fromRGBO(16, 26, 57, 1);
+  static const Color bodyBackground = Color.fromRGBO(6, 13, 38, 1);
+  static const Color grisOscuro = Color.fromRGBO(44, 48, 63, 1);
+  static const Color moradoCard = Color.fromRGBO(r, g, b, opacity)
+
+}
