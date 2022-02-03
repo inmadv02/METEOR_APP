a1558c061358560297be393a192ee1f8c529918f
María Inmaculada Domínguez Vargas <onemayn@outlook.es>
2022-02-03 22:02:40 +0100

    avances

--- meteor_app_proyecto/lib/pages/intro_page.dart
+++ meteor_app_proyecto/lib/pages/intro_page.dart
@@ -10,6 +10,12 @@ class IntroPage extends StatefulWidget {
 class _IntroPageState extends State<IntroPage> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(child: Column(children: []));
+    return SizedBox(
+        child: Column(children: [
+      SizedBox(
+          child: Padding(
+              padding: EdgeInsets.all(6.0),
+              child: Image.asset('assets/images/logo.png')))
+    ]));
   }
 }
--- meteor_app_proyecto/lib/styles.dart
+++ meteor_app_proyecto/lib/styles.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Styles {
+  static const Color blanco = Colors.white;
   static const Color moradoApagado = Color.fromRGBO(130, 140, 174, 1);
   static const Color gris = Color.fromRGBO(204, 207, 218, 1);
   static const Color azulIconsMenu = Color.fromRGBO(85, 126, 174, 1);
   static const Color azulMenu = Color.fromRGBO(16, 26, 57, 1);
   static const Color bodyBackground = Color.fromRGBO(6, 13, 38, 1);
   static const Color grisOscuro = Color.fromRGBO(44, 48, 63, 1);
-  static const Color moradoCard = Color.fromRGBO(r, g, b, opacity)
+  static const Color moradoCard = Color.fromRGBO(167, 180, 224, 1);
 
+  static TextStyle get textTitle => GoogleFonts.getFont(
+        'Roboto Slab',
+        color: Styles.blanco,
+        fontWeight: FontWeight.w600,
+      );
+
+  static TextStyle get textLogo => GoogleFonts.getFont(
+        'Poppins',
+        color: Styles.blanco,
+        fontWeight: FontWeight.w600,
+      );
 }
