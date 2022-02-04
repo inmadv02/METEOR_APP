35f2dd08e3dcb432c298cbb8f155530f55c129f3
María Inmaculada Domínguez Vargas <dominguez.vamar21@triana.salesianos.edu>
2022-02-04 12:29:35 +0100

    Avances de hoy

--- meteor_app_proyecto/lib/pages/intro_page.dart
+++ meteor_app_proyecto/lib/pages/intro_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meteor_app_proyecto/styles.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({Key? key}) : super(key: key);
@@ -8,14 +9,43 @@ class IntroPage extends StatefulWidget {
 }
 
 class _IntroPageState extends State<IntroPage> {
+  List<String> listaCiudades = ["Sevilla", "Londres", "Madrid", "Barcelona"];
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-        child: Column(children: [
+    return Stack(children: [
+      Container(
+          width: MediaQuery.of(context).size.width * 1,
+          height: MediaQuery.of(context).size.height * 1,
+          decoration: const BoxDecoration(color: Styles.bodyBackground)),
       SizedBox(
-          child: Padding(
-              padding: EdgeInsets.all(6.0),
-              child: Image.asset('assets/images/logo.png')))
-    ]));
+          width: MediaQuery.of(context).size.width * 1,
+          height: MediaQuery.of(context).size.height * 1,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            SizedBox(
+                child: Padding(
+                    padding: const EdgeInsets.all(6.0),
+                    child: Image.asset('assets/images/logo.png'))),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(3, 3, 3, 20),
+                child: Text('Meteor', style: Styles.textLogo)),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(3, 3, 3, 20),
+                child: Text('Elige tu ciudad', style: Styles.textTitle)),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(3, 20, 3, 3),
+                child: DropdownButton<String>(
+                  items: listaCiudades.map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                  style: const TextStyle(backgroundColor: Styles.moradoCard),
+                ))
+          ])),
+          ElevatedButton(onPressed: , child:)
+    ]);
   }
 }
